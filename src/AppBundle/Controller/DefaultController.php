43b50ee8edc86712fa9824357a9f9df0a1a38662
Behram ÇELEN <behramcelen@gmail.com>
2016-12-05 04:01:32 +0300

    setup base routing

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,18 +4,57 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/configuration", name="configuration")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        $data = [
+            'name' => 'Baboon Official Theme Server',
+            'categoriesUrl' => $this->generateUrl('categories', [], UrlGeneratorInterface::ABSOLUTE_URL),
+        ];
+
+        return new JsonResponse($data);
+    }
+
+    /**
+     * @Route("/categories", name="categories")
+     */
+    public function categoriesAction(Request $request)
+    {
+        $data = [
+            'categories' => [
+                [
+                    'themesUrl' => $this->generateUrl('category', ['id' => 1], UrlGeneratorInterface::ABSOLUTE_URL),
+                    'categoryName' => 'All',
+                ],
+                [
+                    'themesUrl' => $this->generateUrl('category', ['id' => 2], UrlGeneratorInterface::ABSOLUTE_URL),
+                    'categoryName' => 'Developer Site',
+                ]
+            ],
+        ];
+
+        return new JsonResponse($data);
+    }
+
+    /**
+     * @Route("/category/{id}", name="category")
+     */
+    public function categoryAction(Request $request, $id)
+    {
+        $data[] = [
+            'themeName' => 'Baboon Default Theme',
+            'zipUrl' => 'https://github.com/behram/baboon-default-theme/archive/master.zip',
+            'gitUrl' => 'git@github.com:behram/baboon-default-theme.git',
+        ];
+
+        return new JsonResponse($data);
     }
 }
